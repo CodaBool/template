e3319c64fda3b1166531a029199b6722e7c3da54
Coda Bool <CodaBool@pm.me>
2020-12-22 17:56:31 -0500

    better template with basic navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react'
+
 export default function App() {
   return (
-    <>
-      <h1>nice</h1>
-    </>
+    <div>
+      template
+    </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './global.css'
 
+import App from './App'
+
+/* Navigation */
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+function Navigation() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand>
+        <NavLink to='/' exact>Brand</NavLink>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav>
+          <NavLink to='/' exact>app</NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+
 ReactDOM.render(
-  <>
-    <App />
-  </>,
+  <BrowserRouter>
+    <Navigation />
+    <Switch>
+      <Route path="/" component={App} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 )
