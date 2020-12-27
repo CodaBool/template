f314f13eb8434bd1960f339208842a4a6ac23d08
Coda Bool <CodaBool@pm.me>
2020-12-27 16:42:14 -0500

    react-heroku-setup

--- src/index.js
+++ src/index.js
@@ -8,31 +8,37 @@ import App from './App'
 /* Navigation */
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
+import NavBox from './Components/NavBox'
+import Welcome from './Components/Welcome'
 
 function Navigation() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand>
-        <NavLink to='/' exact>Brand</NavLink>
+        <NavLink to='/' exact>Home</NavLink>
       </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav>
-          <NavLink to='/' exact>app</NavLink>
+          <NavLink to='/' exact activeClassName="selected">app</NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
 }
 
-
 ReactDOM.render(
   <BrowserRouter>
     <Navigation />
-    <Switch>
-      <Route path="/" component={App} />
-    </Switch>
+    <NavBox />
+    <Welcome />
+    <Container>
+      <Switch>
+        <Route path="/" exact component={App} />
+      </Switch>
+    </Container>
   </BrowserRouter>,
   document.getElementById('root')
 )
