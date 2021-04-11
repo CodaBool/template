ae0d4d65905f803ec51c63d8748df4a6a768f47a
CodaBool <CodaBool@pm.me>
2021-04-10 21:37:14 -0400

    slightly more testable setup

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,15 +1,17 @@
-// import dbConnect from '../../util/db'
-// import { User } from '../../models'
+import dbConnect from '../../util/db'
+import { User } from '../../models'
 
 export default async (req, res) => {
   try {
-    // await dbConnect()
-    // let resp = []
-    // await User.find({})
-    //   .then(response => resp = response)
-    //   .catch(err => console.log(err))
-    res.status(200).send('it works')
+    await dbConnect()
+    let resp = []
+    await User.find({})
+      .then(response => resp = response)
+      .catch(err => console.log(err))
+    console.log(resp)
+    res.status(200).send(resp)
   } catch (err) {
+    console.log(err)
     res.status(500).send(err)
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import axios from 'axios'
 
 export default function index() {
 
   function handleClick() {
     console.log('click')
+    axios.get('/api/test')
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data))
   }
 
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={handleClick} />
+      <button onClick={handleClick}>Click</button>
     </div>
   )
 }
